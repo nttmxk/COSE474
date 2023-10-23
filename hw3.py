4914c001ea16d23a47fef1003a5145d725b580b2
Julien <nttmxk@korea.ac.kr>
2023-10-24 03:10:14 +0900

    refactor: reformat code Ctrl+Alt+L

--- hw3.py
+++ hw3.py
@@ -3,47 +3,48 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 class nn_linear_layer:
-    
+
     # linear layer.
     # randomly initialized by creating matrix W and bias b
     def __init__(self, input_size, output_size, std=1):
-        self.W = np.random.normal(0,std,(output_size,input_size))
-        self.b = np.random.normal(0,std,(output_size,1))
-    
+        self.W = np.random.normal(0, std, (output_size, input_size))
+        self.b = np.random.normal(0, std, (output_size, 1))
+
     ######
     ## Q1
-    def forward(self,x):
-        return x@self.W.T+self.b.T
-    
+    def forward(self, x):
+        return x @ self.W.T + self.b.T
+
     ######
     ## Q2
     ## returns three parameters
-    def backprop(self,x,dLdy):
-        dLdW = dLdy.T@x
+    def backprop(self, x, dLdy):
+        dLdW = dLdy.T @ x
         dLdb = np.sum(dLdy.T, axis=1, keepdims=True).T
-        dLdx = dLdy@self.W
-        return dLdW,dLdb,dLdx
+        dLdx = dLdy @ self.W
+        return dLdW, dLdb, dLdx
 
-    def update_weights(self,dLdW,dLdb):
-
+    def update_weights(self, dLdW, dLdb):
         # parameter update
-        self.W=self.W+dLdW
-        self.b=self.b+dLdb
+        self.W = self.W + dLdW
+        self.b = self.b + dLdb
+
 
 class nn_activation_layer:
-    
+
     def __init__(self):
         pass
-    
+
     ######
     ## Q3
-    def forward(self,x):
+    def forward(self, x):
         return 1 / (1 + np.exp(-x))
-    
+
     ######
     ## Q4
-    def backprop(self,x,dLdy):
+    def backprop(self, x, dLdy):
         y = self.forward(x)
         dsig = y * (1 - y)
         dLdx = dLdy * dsig
@@ -53,65 +54,68 @@ class nn_activation_layer:
 class nn_softmax_layer:
     def __init__(self):
         pass
+
     ######
     ## Q5
-    def forward(self,x):
+    def forward(self, x):
         exps = np.exp(x - np.max(x, axis=1, keepdims=True))
         return exps / np.sum(exps, axis=1, keepdims=True)
-    
+
     ######
     ## Q6
-    def backprop(self,x,dLdy):
+    def backprop(self, x, dLdy):
         # y = self.forward(x)
         # dLdx = y * (dLdy - np.sum(dLdy * y, axis=1, keepdims=True))
         return dLdy
 
+
 class nn_cross_entropy_layer:
     def __init__(self):
         pass
-        
+
     ######
     ## Q7
-    def forward(self,x,y):
+    def forward(self, x, y):
         lx = -np.log(x[range(x.shape[0]), y.reshape(-1)])
         loss = np.sum(lx) / x.shape[0]
         return loss
-        
+
     ######
     ## Q8
-    def backprop(self,x,y):
+    def backprop(self, x, y):
         dLdx = x.copy()
-        dLdx[range(x.shape[0]), y.reshape(x.shape[0],)] -= 1
+        dLdx[range(x.shape[0]), y.reshape(x.shape[0], )] -= 1
         dLdx /= x.shape[0]
         return dLdx
 
+
 # number of data points for each of (0,0), (0,1), (1,0) and (1,1)
-num_d=5
+num_d = 5
 
 # number of test runs
-num_test=40
+num_test = 40
 
 ## Q9. Hyperparameter setting
 ## learning rate (lr)and number of gradient descent steps (num_gd_step)
 ## This part is not graded (there is no definitive answer).
 ## You can set this hyperparameters through experiments.
-lr=0.2
-num_gd_step=10000
+lr = 0.2
+num_gd_step = 10000
 
 # dataset size
-batch_size=4*num_d
+batch_size = 4 * num_d
 
 # number of classes is 2
-num_class=2
+num_class = 2
 
 # variable to measure accuracy
-accuracy=0
+accuracy = 0
 
 # set this True if want to plot training data
-show_train_data=False
+show_train_data = False
 
 # set this True if want to plot loss over gradient descent iteration
-show_loss=True
+show_loss = True
 
 ################
 # create training data
@@ -168,24 +172,24 @@ loss_out = np.zeros((num_gd_step))
 ################
 
 for i in range(num_gd_step):
-    
+
     # fetch data
     x_train = x_train_d
     y_train = y_train_d
-    
+
     # create one-hot vectors from the ground truth labels
     y_onehot = np.zeros((batch_size, num_class))
     y_onehot[range(batch_size), y_train.reshape(batch_size, )] = 1
-    
+
     ################
     # forward pass
-    
+
     # hidden layer
     # linear
     l1_out = layer1.forward(x_train)
     # activation
     a1_out = act.forward(l1_out)
-    
+
     # output layer
     # linear
     l2_out = layer2.forward(a1_out)
@@ -193,7 +197,7 @@ for i in range(num_gd_step):
     smax_out = smax.forward(l2_out)
     # cross entropy loss
     loss_out[i] = cent.forward(smax_out, y_train)
-    
+
     ################
     # perform backprop
     # output layer
@@ -201,21 +205,21 @@ for i in range(num_gd_step):
     b_cent_out = cent.backprop(smax_out, y_train)
     # softmax
     b_nce_smax_out = smax.backprop(l2_out, b_cent_out)
-    
+
     # linear
     b_dLdW_2, b_dLdb_2, b_dLdx_2 = layer2.backprop(x=a1_out, dLdy=b_nce_smax_out)
-    
+
     # backprop, hidden layer
     # activation
     b_act_out = act.backprop(x=l1_out, dLdy=b_dLdx_2)
     # linear
     b_dLdW_1, b_dLdb_1, b_dLdx_1 = layer1.backprop(x=x_train, dLdy=b_act_out)
-    
+
     ################
     # update weights: perform gradient descent
     layer2.update_weights(dLdW=-b_dLdW_2 * lr, dLdb=-b_dLdb_2.T * lr)
     layer1.update_weights(dLdW=-b_dLdW_1 * lr, dLdb=-b_dLdb_1.T * lr)
-    
+
     if (i + 1) % 2000 == 0:
         print('gradient descent iteration:', i + 1)
 
@@ -235,19 +239,19 @@ if (show_loss):
 num_test = 100
 
 for j in range(num_test):
-    
+
     predicted = np.ones((4,))
-    
+
     # dispersion of test data
     sig_t = 1e-2
-    
+
     # generate test data
     # generate 4 samples, each sample nearby (1,1), (0,0), (1,0), (0,1) respectively
-    t11 = np.random.multivariate_normal((1,1), sig_t**2*np.eye(2), 1)
-    t00 = np.random.multivariate_normal((0,0), sig_t**2*np.eye(2), 1)
-    t10 = np.random.multivariate_normal((1,0), sig_t**2*np.eye(2), 1)
-    t01 = np.random.multivariate_normal((0,1), sig_t**2*np.eye(2), 1)
-    
+    t11 = np.random.multivariate_normal((1, 1), sig_t ** 2 * np.eye(2), 1)
+    t00 = np.random.multivariate_normal((0, 0), sig_t ** 2 * np.eye(2), 1)
+    t10 = np.random.multivariate_normal((1, 0), sig_t ** 2 * np.eye(2), 1)
+    t01 = np.random.multivariate_normal((0, 1), sig_t ** 2 * np.eye(2), 1)
+
     # predicting label for test sample nearby (1,1)
     l1_out = layer1.forward(t11)
     a1_out = act.forward(l1_out)
@@ -255,7 +259,7 @@ for j in range(num_test):
     smax_out = smax.forward(l2_out)
     predicted[0] = np.argmax(smax_out)
     print('softmax out for (1,1)', smax_out, 'predicted label:', int(predicted[0]))
-    
+
     # predicting label for test sample nearby (0,0)
     l1_out = layer1.forward(t00)
     a1_out = act.forward(l1_out)
@@ -263,7 +267,7 @@ for j in range(num_test):
     smax_out = smax.forward(l2_out)
     predicted[1] = np.argmax(smax_out)
     print('softmax out for (0,0)', smax_out, 'predicted label:', int(predicted[1]))
-    
+
     # predicting label for test sample nearby (1,0)
     l1_out = layer1.forward(t10)
     a1_out = act.forward(l1_out)
@@ -271,7 +275,7 @@ for j in range(num_test):
     smax_out = smax.forward(l2_out)
     predicted[2] = np.argmax(smax_out)
     print('softmax out for (1,0)', smax_out, 'predicted label:', int(predicted[2]))
-    
+
     # predicting label for test sample nearby (0,1)
     l1_out = layer1.forward(t01)
     a1_out = act.forward(l1_out)
@@ -279,18 +283,12 @@ for j in range(num_test):
     smax_out = smax.forward(l2_out)
     predicted[3] = np.argmax(smax_out)
     print('softmax out for (0,1)', smax_out, 'predicted label:', int(predicted[3]))
-    
+
     print('total predicted labels:', predicted.astype('uint8'))
-    
+
     accuracy += (predicted[0] == 0) & (predicted[1] == 0) & (predicted[2] == 1) & (predicted[3] == 1)
-    
+
     if (j + 1) % 10 == 0:
         print('test iteration:', j + 1)
 
 print('accuracy:', accuracy / num_test * 100, '%')
-
-
-
-
-
-
