63a751eff0e53f32dbc582c1d283b1a521794e67
julien <nttmxk@korea.ac.kr>
2023-11-11 16:00:24 +0900

    refactor: Reformat code
    Ctrl+Alt+L
    Remove some comments

--- HW4/hw4.py
+++ HW4/hw4.py
@@ -1,7 +1,5 @@
 """
-
 HW4
-
 """
 
 import torch
@@ -17,8 +15,8 @@ function view_as_windows
 
 """
 
-def view_as_windows(arr_in, window_shape, step=1):
 
+def view_as_windows(arr_in, window_shape, step=1):
     # -- basic checks on arguments
     if not torch.is_tensor(arr_in):
         raise TypeError("`arr_in` must be a pytorch tensor")
@@ -54,29 +52,25 @@ def view_as_windows(arr_in, window_shape, step=1):
     indexing_strides = arr_in[slices].stride()
 
     win_indices_shape = torch.div(arr_shape - window_shape
-                          , torch.tensor(step), rounding_mode = 'floor') + 1
-    
+                                  , torch.tensor(step), rounding_mode='floor') + 1
+
     new_shape = tuple(list(win_indices_shape) + list(window_shape))
     strides = tuple(list(indexing_strides) + list(window_strides))
 
     arr_out = torch.as_strided(arr_in, size=new_shape, stride=strides)
     return arr_out
 
-#######
-# if necessary, you can define additional functions which help your implementation,
-# and import proper libraries for those functions.
-#######
-
 
 debug_flag = False
+
 
 class nn_convolutional_layer:
 
     def __init__(self, f_height, f_width, input_size, in_ch_size, out_ch_size):
-        
+
         # Xavier init
         self.W = torch.normal(0, 1 / math.sqrt((in_ch_size * f_height * f_width / 2)),
-                                  size=(out_ch_size, in_ch_size, f_height, f_width))
+                              size=(out_ch_size, in_ch_size, f_height, f_width))
         self.b = 0.01 + torch.zeros(size=(1, out_ch_size, 1, 1))
 
         self.W.requires_grad = True
@@ -125,20 +119,11 @@ class nn_convolutional_layer:
         out = torch.matmul(windows_reshaped, w_reshaped.T).permute(0, 3, 1, 2)
         return out
 
-    
-
-    #######
-    ## If necessary, you can define additional class methods here
-    #######
-
 
 class nn_max_pooling_layer:
     def __init__(self, pool_size, stride):
         self.stride = stride
         self.pool_size = pool_size
-        #######
-        ## If necessary, you can define additional class variables here
-        #######
 
     #######
     # Q2. Complete this method
@@ -158,10 +143,6 @@ class nn_max_pooling_layer:
         out = windows.max(-1)[0].max(-1)[0]
         return out
 
-    
-    #######
-    ## If necessary, you can define additional class methods here
-    #######
 
 """
 TESTING 
@@ -187,7 +168,6 @@ if __name__ == "__main__":
     err_fwd = 0
     err_pool = 0
 
-
     # for reproducibility
     # torch.manual_seed(0)
 
@@ -198,48 +178,47 @@ if __name__ == "__main__":
     for i in range(num_test):
         # create convolutional layer object
         cnv = nn_convolutional_layer(filter_height, filter_width, input_size,
-                                   in_ch_size, num_filters)
+                                     in_ch_size, num_filters)
 
         # test conv layer from torch.nn for reference
         test_conv_layer = nn.Conv2d(in_channels=in_ch_size, out_channels=num_filters,
-                                kernel_size = (filter_height, filter_width))
-        
+                                    kernel_size=(filter_height, filter_width))
+
         # test input
         x = torch.normal(0, 1, (batch_size, in_ch_size, input_size, input_size))
-        
+
         with torch.no_grad():
-            
             out = cnv.forward(x)
-            W,b = cnv.get_weights()
+            W, b = cnv.get_weights()
             test_conv_layer.weight = nn.Parameter(W)
             test_conv_layer.bias = nn.Parameter(torch.squeeze(b))
             test_out = test_conv_layer(x)
-            
-            err=torch.norm(test_out - out)/torch.norm(test_out)
-            err_fwd+= err
-    
+
+            err = torch.norm(test_out - out) / torch.norm(test_out)
+            err_fwd += err
+
     stride = 2
     pool_size = 2
-    
+
     print('pooling test')
     for i in range(num_test):
         # create pooling layer object
         mpl = nn_max_pooling_layer(pool_size=pool_size, stride=stride)
-        
+
         # test pooling layer from torch.nn for reference
-        test_pooling_layer = nn.MaxPool2d(kernel_size=(pool_size,pool_size), stride=stride)
-        
+        test_pooling_layer = nn.MaxPool2d(kernel_size=(pool_size, pool_size), stride=stride)
+
         # test input
         x = torch.normal(0, 1, (batch_size, in_ch_size, input_size, input_size))
-        
+
         with torch.no_grad():
             out = mpl.forward(x)
             test_out = test_pooling_layer(x)
-            
-            err=torch.norm(test_out - out)/torch.norm(test_out)
-            err_pool+= err
+
+            err = torch.norm(test_out - out) / torch.norm(test_out)
+            err_pool += err
 
     # reporting accuracy results.
     print('accuracy results')
-    print('forward accuracy', 100 - err_fwd/num_test*100, '%')
-    print('pooling accuracy', 100 - err_pool/num_test*100, '%')
+    print('forward accuracy', 100 - err_fwd / num_test * 100, '%')
+    print('pooling accuracy', 100 - err_pool / num_test * 100, '%')
