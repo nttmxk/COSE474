e9ed63ae8b0a81625301e6b230af510e7eff7952
Julien <nttmxk@korea.ac.kr>
2023-12-24 02:42:05 +0900

    refactor: apply Ctrl+Alt+L

--- HW7/hw7.py
+++ HW7/hw7.py
@@ -9,7 +9,6 @@ from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data.dataset import random_split
 
-
 from tqdm import tqdm
 
 import importlib
@@ -18,8 +17,6 @@ from datetime import datetime as dt
 import time
 
 import imdb_voc
-
-
 
 root = './'
 
@@ -35,8 +32,9 @@ You can implement any necessary methods.
 
 """
 
+
 class MultiHeadAttention(nn.Module):
-    def __init__(self, d_model, d_Q, d_K, d_V, numhead, dropout):    
+    def __init__(self, d_model, d_Q, d_K, d_V, numhead, dropout):
         super().__init__()
         # Q1. Implement
         self.d_Q = d_Q
@@ -77,16 +75,17 @@ class MultiHeadAttention(nn.Module):
             mask[index, length:] = 1
         return mask.unsqueeze(1)
 
+
 class TF_Encoder_Block(nn.Module):
-    def __init__(self, d_model, d_ff, numhead, dropout):    
-      super().__init__()
-    
-      # Q3. Implment constructor for transformer encoder block
+    def __init__(self, d_model, d_ff, numhead, dropout):
+        super().__init__()
+
+        # Q3. Implment constructor for transformer encoder block
 
     def forward(self, x, src_batch_lens):
-      
         # Q4. Implment forward function for transformer encoder block
         return out
+
 
 """
 Positional encoding
@@ -94,35 +93,36 @@ PE(pos,2i) = sin(pos/10000**(2i/dmodel))
 PE(pos,2i+1) = cos(pos/10000**(2i/dmodel))
 """
 
+
 def PosEncoding(t_len, d_model):
     i = torch.tensor(range(d_model))
     pos = torch.tensor(range(t_len))
     POS, I = torch.meshgrid(pos, i)
-    PE = (1-I % 2)*torch.sin(POS/10**(4*I/d_model)) + (I%2)*torch.cos(POS/10**(4*(I-1)/d_model))
+    PE = (1 - I % 2) * torch.sin(POS / 10 ** (4 * I / d_model)) + (I % 2) * torch.cos(
+        POS / 10 ** (4 * (I - 1) / d_model))
     return PE
+
 
 class TF_Encoder(nn.Module):
     def __init__(self, vocab_size, d_model,
-                 d_ff, numlayer, numhead, dropout):    
+                 d_ff, numlayer, numhead, dropout):
         super().__init__()
-        
+
         self.numlayer = numlayer
-        self.src_embed  = nn.Embedding(num_embeddings=vocab_size, embedding_dim=d_model)
-        self.dropout=nn.Dropout(dropout)
+        self.src_embed = nn.Embedding(num_embeddings=vocab_size, embedding_dim=d_model)
+        self.dropout = nn.Dropout(dropout)
 
         # Q5. Implement a sequence of numlayer encoder blocks
-        
+
     def forward(self, x, src_batch_lens):
+        x_embed = self.src_embed(x)
+        x = self.dropout(x_embed)
+        p_enc = PosEncoding(x.shape[1], x.shape[2]).to(dev)
+        x = x + p_enc
 
-      x_embed = self.src_embed(x)
-      x = self.dropout(x_embed)
-      p_enc = PosEncoding(x.shape[1], x.shape[2]).to(dev)
-      x = x + p_enc
-        
-      # Q6. Implement: forward over numlayer encoder blocks
-      
-      return out
+        # Q6. Implement: forward over numlayer encoder blocks
 
+        return out
 
 
 """
@@ -131,38 +131,39 @@ main model
 
 """
 
+
 class sentiment_classifier(nn.Module):
-    
-    def __init__(self, enc_input_size, 
+
+    def __init__(self, enc_input_size,
                  enc_d_model,
                  enc_d_ff,
                  enc_num_layer,
                  enc_num_head,
                  dropout,
-                ):    
+                 ):
         super().__init__()
-        
-        self.encoder = TF_Encoder(vocab_size = enc_input_size,
-                                  d_model = enc_d_model, d_ff=enc_d_ff,
+
+        self.encoder = TF_Encoder(vocab_size=enc_input_size,
+                                  d_model=enc_d_model, d_ff=enc_d_ff,
                                   numlayer=enc_num_layer, numhead=enc_num_head,
                                   dropout=dropout)
 
         self.classifier = nn.Sequential(
-            nn.AdaptiveAvgPool2d((1,None)),
+            nn.AdaptiveAvgPool2d((1, None)),
             nn.Dropout(dropout),
-            nn.Linear(in_features = enc_d_model, out_features=enc_d_model),
+            nn.Linear(in_features=enc_d_model, out_features=enc_d_model),
             nn.ReLU(),
             nn.Dropout(dropout),
-            nn.Linear(in_features = enc_d_model, out_features = 1)
+            nn.Linear(in_features=enc_d_model, out_features=1)
         )
-          
-   
+
     def forward(self, x, x_lens):
-        src_ctx = self.encoder(x, src_batch_lens = x_lens)
+        src_ctx = self.encoder(x, src_batch_lens=x_lens)
         # size should be (b,)
         out_logits = self.classifier(src_ctx).flatten()
 
         return out_logits
+
 
 """
 
@@ -206,17 +207,16 @@ SRC_PAD_IDX = src_word_dict['<PAD>']
 show_sample_reviews = True
 
 if show_sample_reviews:
-    
-    sample_text, sample_lab = next(iter(train_dataloader))
-    slist=[]
 
-    for stxt in sample_text[:4]: 
+    sample_text, sample_lab = next(iter(train_dataloader))
+    slist = []
+
+    for stxt in sample_text[:4]:
         slist.append([src_idx_dict[j] for j in stxt])
 
     for j, s in enumerate(slist):
-        print('positive' if sample_lab[j]==1 else 'negative')
-        print(' '.join([i for i in s if i != '<PAD>'])+'\n')
-
+        print('positive' if sample_lab[j] == 1 else 'negative')
+        print(' '.join([i for i in s if i != '<PAD>']) + '\n')
 
 """
 
@@ -224,7 +224,7 @@ model
 
 """
 
-enc_vocab_size = len(src_word_dict) # counting eof, one-hot vector goes in
+enc_vocab_size = len(src_word_dict)  # counting eof, one-hot vector goes in
 
 # Set hyperparam (model size)
 # examples: model & ff dim - 8, 16, 32, 64, 128, numhead, numlayer 1~4
@@ -240,11 +240,11 @@ enc_num_layer= ...
 DROPOUT=0.1
 
 model = sentiment_classifier(enc_input_size=enc_vocab_size,
-                         enc_d_model = enc_d_model,     
-                         enc_d_ff = enc_d_ff, 
-                         enc_num_head = enc_num_head, 
-                         enc_num_layer = enc_num_layer,
-                         dropout=DROPOUT) 
+                             enc_d_model=enc_d_model,
+                             enc_d_ff=enc_d_ff,
+                             enc_num_head=enc_num_head,
+                             enc_num_layer=enc_num_layer,
+                             dropout=DROPOUT)
 
 model = model.to(dev)
 
@@ -259,7 +259,7 @@ optimizer
 
 lr = ...
 
-optimizer = torch.optim.Adam(model.parameters(), lr = lr)
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
 criterion = nn.BCEWithLogitsLoss()
 
@@ -274,38 +274,38 @@ auxiliary functions
 def get_lens_from_tensor(x):
     # lens (batch, t)
     lens = torch.ones_like(x).long()
-    lens[x==SRC_PAD_IDX]=0
+    lens[x == SRC_PAD_IDX] = 0
     return torch.sum(lens, dim=-1)
+
 
 def get_binary_metrics(y_pred, y):
     # find number of TP, TN, FP, FN
-    TP=sum(((y_pred == 1)&(y==1)).type(torch.int32))
-    FP=sum(((y_pred == 1)&(y==0)).type(torch.int32))
-    TN=sum(((y_pred == 0)&(y==0)).type(torch.int32))
-    FN=sum(((y_pred == 0)&(y==1)).type(torch.int32))
-    accy = (TP+TN)/(TP+FP+TN+FN)
-            
-    recall = TP/(TP+FN) if TP+FN!=0 else 0
-    prec = TP/(TP+FP) if TP+FP!=0 else 0
-    f1 = 2*recall*prec/(recall+prec) if recall+prec !=0 else 0
-    
+    TP = sum(((y_pred == 1) & (y == 1)).type(torch.int32))
+    FP = sum(((y_pred == 1) & (y == 0)).type(torch.int32))
+    TN = sum(((y_pred == 0) & (y == 0)).type(torch.int32))
+    FN = sum(((y_pred == 0) & (y == 1)).type(torch.int32))
+    accy = (TP + TN) / (TP + FP + TN + FN)
+
+    recall = TP / (TP + FN) if TP + FN != 0 else 0
+    prec = TP / (TP + FP) if TP + FP != 0 else 0
+    f1 = 2 * recall * prec / (recall + prec) if recall + prec != 0 else 0
+
     return accy, recall, prec, f1
+
 
 """
 
 train/validation
 
-""" 
+"""
 
 
 def train(model, dataloader, optimizer, criterion, clip):
-
     model.train()
 
     epoch_loss = 0
 
     for i, batch in enumerate(dataloader):
-
         src = batch[0].to(dev)
         trg = batch[1].float().to(dev)
 
@@ -316,12 +316,11 @@ def train(model, dataloader, optimizer, criterion, clip):
 
         x_lens = get_lens_from_tensor(src).to(dev)
 
-        output = model(x=src, x_lens=x_lens) 
-
+        output = model(x=src, x_lens=x_lens)
 
         output = output.contiguous().view(-1)
         trg = trg.contiguous().view(-1)
-        
+
         loss = criterion(output, trg)
 
         loss.backward()
@@ -334,33 +333,32 @@ def train(model, dataloader, optimizer, criterion, clip):
 
     return epoch_loss / len(dataloader)
 
-def evaluate(model, dataloader, criterion):
 
+def evaluate(model, dataloader, criterion):
     model.eval()
-    
+
     epoch_loss = 0
-    
-    epoch_accy =0
-    epoch_recall =0
-    epoch_prec =0
-    epoch_f1 =0
+
+    epoch_accy = 0
+    epoch_recall = 0
+    epoch_prec = 0
+    epoch_f1 = 0
 
     with torch.no_grad():
         for i, batch in enumerate(dataloader):
-
             src = batch[0].to(dev)
             trg = batch[1].float().to(dev)
 
             x_lens = get_lens_from_tensor(src).to(dev)
 
-            output = model(x=src, x_lens=x_lens) 
+            output = model(x=src, x_lens=x_lens)
 
             output = output.contiguous().view(-1)
             trg = trg.contiguous().view(-1)
 
             loss = criterion(output, trg)
-            
-            accy, recall, prec, f1 = get_binary_metrics((output>=0).long(), trg.long())
+
+            accy, recall, prec, f1 = get_binary_metrics((output >= 0).long(), trg.long())
             epoch_accy += accy
             epoch_recall += recall
             epoch_prec += prec
@@ -369,15 +367,17 @@ def evaluate(model, dataloader, criterion):
             epoch_loss += loss.item()
 
     # show accuracy
-    print(f'\tAccuracy: {epoch_accy/(len(dataloader)):.3f}')
-    
+    print(f'\tAccuracy: {epoch_accy / (len(dataloader)):.3f}')
+
     return epoch_loss / len(dataloader)
+
 
 def epoch_time(start_time, end_time):
     elapsed_time = end_time - start_time
     elapsed_mins = int(elapsed_time / 60)
     elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
     return elapsed_mins, elapsed_secs
+
 
 """
 
@@ -405,9 +405,9 @@ for epoch in range(N_EPOCHS):
         best_valid_loss = valid_loss
         torch.save(model.state_dict(), 'model.pt')
 
-    print(f'Epoch: {epoch+1:02} | Time: {epoch_mins}m {epoch_secs}s')
+    print(f'Epoch: {epoch + 1:02} | Time: {epoch_mins}m {epoch_secs}s')
     print(f'\tTrain Loss: {train_loss:.3f} | Val. Loss: {valid_loss:.3f}')
-        
+
 """
 
 Test loop
